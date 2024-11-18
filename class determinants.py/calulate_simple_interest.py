f1a6a7ebd54dc56c0676a500273ce1a819e0e827
joyce835 <dorcasolayemi495@gmail.com>
2024-11-18 10:57:46 -0700

    documented a function

--- class determinants.py/calulate_simple_interest.py
+++ class determinants.py/calulate_simple_interest.py
@@ -2,6 +2,9 @@
 #that calulates simple interest
 
 def simple_interest(prinical,rate,time):
+    """
+    determine the simple interset base on prinical, rate,time
+    """
     print(prinical * rate * time)/100
 prinical=int(input("enter your princical amount:"))
 rate=int(input("enter your rate amount:"))
