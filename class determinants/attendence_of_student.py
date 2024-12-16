851088243f8976bc2ffafcda667b3e52143e2d96
joyce835 <dorcasolayemi495@gmail.com>
2024-12-16 08:38:34 -0700

    Correction of project

--- class determinants/attendence_of_student.py
+++ class determinants/attendence_of_student.py
@@ -1,47 +1,89 @@
 #attendence of student
-#writing a python project whereby writing the number of times a student was present or absent
-#calulating the attendence report sheet by using the offical attendence mark
-#and grade the student by the number of times the student was present or absent.
-
-name=["joy","peace","john","alice"]
-name_of_student=input("enter your name:")
-if name_of_student in name:
-        print("Student name found!")
-elif name_of_student not in name:
-        print("student name is not found!")
-        exit()
-else:
-     print("error")
-
-name_of_class=input("enter the name of your class:")
-student_school=input("enter the name of school")
+#writing a python project whereby creating the a dictionary of each student 
+#searching for a particular student in a list
+#if name found print the things that associate with that particular student and import a module called datetime
+# To record the searching and give the hour,year,mintues,seconds of searching it.
+import datetime  #importing time module to record the start time and the end time of searching the student record.
+print("\nWelcome to a Attendence reportsheet")
+#Creating a dictionary for each of the student
+student_joy={
+    "name":"Joy",
+    "class": "Physics",
+    "Department":"Science",
+    "School":"Sunshine High School",
+    "Attendance":120
+}
 
 
-offical_attendence_mark=120
-student_times_absent=int(input("enter the number of times absent:"))
+student_peace={
+    "name":"Peace",
+    "class": "Business",
+    "Department":"Commerce",
+    "School":"Townsend College",
+    "Attendance":70
+}
+
+student_john={
+    "name":"John",
+    "class": "English",
+    "Department":"Art",
+    "School":"Rivers highschool",
+    "Attendance":80
+}
+
+student_alice={
+    "name":"Alice",
+    "class": "Computer",
+    "Department":"Ict",
+    "School":"Hilltop school",
+    "Attendance":90
+}
+#Total attendence mark for the student
+max_attendence=120
+#Storing the names of the student in a list
+records=[student_joy,student_peace,student_john,student_alice]
+name=input("enter your name:  ")# asking the student to input their name
+found=False
+#Acessing the student name, if the student name is part of the of the dictionary above
+for record in records:
+    if name==record.get("name"):
+        found=True #if it's part of the dictionary print found and access the things that are relating to the student
+        now=datetime.datetime.now() #using datetimes to record the research of the student
+        print(now)
+        print("name found")
+        print("-------------")
+        print("\nStudent summary")
+        print(record.get("name"))
+        print(record.get("class"))
+        print(record.get("Department"))
+        print(record.get("School"))
+        attendance=record.get("Attendance")
+        print(attendance)
+# grading the attendance of the student base on their attendence mark
+        if attendance < max_attendence:
+            print("Not punctual")
+        else:
+            print("punctual")
+    
+        break     
+
+if found !=True: #if the name is not part of the dictionary above print not found
+    print("name not found")
 
 
-student_times_present = offical_attendence_mark - student_times_absent
-if student_times_absent > offical_attendence_mark:
-       exit()
-elif student_times_absent < offical_attendence_mark:
-       print("continue marking")
-else:
-       print("error")
-       
-print (f'Total attendance is 120 \n Your total attendance present is {student_times_present} days')
 
-if student_times_present == 120:
-       print("promoted")
-elif student_times_present<= 119 and student_times_present >=60:
-       print("can still be promoted")
-elif student_times_present <= 59 and student_times_present>=30:
-       print("can still be considered")
-elif student_times_present <= 29 and student_times_present >=10:
-       print("Attendance too poor for promotion")
-else:
-       print("repeat class")
-        
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
